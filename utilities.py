27e87b3cdf69c7575f3e98f0fed1994d80330b29
Arthur Tilley <aetilley@gmail.com>
2017-04-29 11:11:33 -0700

    utilities minor comment edit

--- utilities.py
+++ utilities.py
@@ -12,10 +12,8 @@ def one_hot(object, object_vector):
     a = np.array(object_vector) == object
     return a.astype(float)
 
-
-#Weight generating functions                                                                                         
+#Weight generating functions                                                                               
 def weight_variables(shape):
-    #Shape will be used for ConvLayer so probably [filter_width, in_channels, out_channels]                         
     initial = tf.truncated_normal(shape, stddev=0.05)
     return tf.Variable(initial)
 
@@ -23,14 +21,9 @@ def bias_variables(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-#ConvLayer generating function                                                                                        
-# x shape is [batch, in_width, in_channels]
-# W shape is [filter_width, in_channels, out_channels]                                                          
-
+#Layer generating wrappers
 def conv(x, W):
   return tf.nn.conv1d(x, W, stride=1, padding='SAME')
-
-#For max pool, function assumes 2d input.                                                                   
 
 def pool(x, pool_size):
     return tf.nn.pool(x, window_shape =[pool_size], pooling_type = "MAX",\
