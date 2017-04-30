783f65d9b825dcf04e3219d5ba599599b23a817b
Arthur Tilley <aetilley@gmail.com>
2017-04-29 18:35:32 -0700

    fixed newline bug in get_guten_sents.py

--- get_guten_sents.py
+++ get_guten_sents.py
@@ -1,12 +1,18 @@
 import nltk
 import io
 
-for fileid in gut.fileids():
-    write_file_name = "gutenberg_"+fileid[-4]+"_sents.txt"
-    with io.open("guten_data/"+write_file_name, 'w', encoding = 'UTF-8') as f:
-        raw_text = gut.raw(fileids = fileid)
-        sent_string_list = nltk.sent_tokenize(raw_text)
-        for sent in sent_string_list:
-        no_newline = sent.replace("\n", "")
-        f.write(no_newline+"\n")
+
+def main():
+
+    gut = nltk.corpus.gutenberg
+    for fileid in gut.fileids():
+        write_file_name = "gutenberg_"+fileid[:-4]+"_sents.txt"
+        with io.open("guten_data/"+write_file_name, 'w', encoding = 'UTF-8') as f:
+            raw_text = gut.raw(fileids = fileid)
+            sent_string_list = nltk.sent_tokenize(raw_text)
+            for sent in sent_string_list:
+                no_newline = sent.replace("\n", " ")
+                if no_newline[-1] == " ":
+                    no_newline = no_newline[:-1]
+                f.write(no_newline+"\n")
 
